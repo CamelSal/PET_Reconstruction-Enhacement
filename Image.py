d83531d256fc161aaa87a795465eeeb2423d3308
CamelSal <50183653+CamelSal@users.noreply.github.com>
2019-06-15 14:33:03 -0400

    Update Image.py

--- Image.py
+++ Image.py
@@ -13,32 +13,34 @@ for i in theta: #loads each file for the representing angel
     print(x)
     data1.append(x)
 
-sinogram= np.column_stack(data1) # rearranges the data to form the sinogram
+sinogram= np.column_stack(data1) # rearranges the data to form a projection sinogram obataned fro the files
 
 
 y= loadfile("Scan4_0Deg.txt",0) + 10
 reconstruction = iradon_sart(sinogram, theta=theta,relaxation=0.15) # reconstructed the image obatianed from the sinogram 
-# variable to generated the indidual curves of counts
-errodata = data**(1/2)
+# variable to generated the indidual curves of coicidence signal 
+errodata = data**(1/2) # error of each measurement which is proportional to the sqaureroot of the count
 p = [0,1,2,3,4,5,6,7]
 color = ['b', 'g', 'r', 'c', 'm', 'y', 'k','silver','navy','chocolate','lime']
 
 
-
+# figure provides 3 different subplots
 fig = plt.figure(figsize=(25, 10))
 ax1 = plt.subplot2grid((2, 4),(0,2),colspan=2)
 ax2 = plt.subplot2grid((2, 4),(0,0),colspan=2, rowspan=2)
 ax3 = plt.subplot2grid((2, 4),(1,2),colspan=2)
 
-
+#this plots the projection sinogram obtained from the multiple scans at different angels
 ax1.imshow(sinogram, cmap='gray',extent=[0,t-1,-10,10],aspect='auto',interpolation='quadric')
 ax1.set_ylabel('Projection Axis (mm)')
 ax1.set_title("Projection Sinogram")
 ax1.set_xlabel("Projection Angles (deg)")
+# the recorstucted projection of the image done via an inversed radon trasformation
 ax2.imshow(reconstruction, cmap='gray',aspect='auto')
 ax2.set_xticks([])
 ax2.set_yticks([])
 ax2.set_title("Reconstruction",fontsize=15)
+#plots of the projection of location vs count at specific angels whit their errors 
 for i in p:
     ax3.errorbar(y,data[:, i],color = color[i],yerr=errodata[:, i])
 ax3.legend(theta[(p)],ncol=2)
